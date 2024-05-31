ca7fb5a4d8e208dbd4ed2e8cd911d26812993d96
NoMeLlamoDante <eo.zm24@outlook.com>
2024-05-30 19:18:00 -0600

    ejemplo put

--- FastApi/users.py
+++ FastApi/users.py
@@ -49,3 +49,13 @@ async def user(user: User):
     else:
         users_list.append(user)
 
+@app.put("/user/")
+async def user(user: User):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    if not found:
+        return {"error": "no se ha actualizado el usuario"}
+    
