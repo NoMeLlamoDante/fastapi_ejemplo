e866a2f13e66ac18d4b7ec0e059e07299befe817
NoMeLlamoDante <eo.zm24@outlook.com>
2024-06-04 12:07:54 -0600

    ejemplo de acceso a datos de usuario

--- FastApi/routers/basic_auth_users.py
+++ FastApi/routers/basic_auth_users.py
@@ -34,24 +34,35 @@ class UserDB(User):
     password: str
 
 
+def search_user_db(username: str):
+    if username in users_db:
+        return UserDB(**users_db[username])
+    
 def search_user(username: str):
     if username in users_db:
-        user_dict = users_db[username]
-        return UserDB(**user_dict)
-
+        return User(**users_db[username])
+    
 async def current_user(token: str = Depends(oauth2)):
     user =  search_user(token)
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="no autorizado")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="no autorizado", 
+            headers={"WWW-Authenticate": "Bearer"})
+    if user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Usuario Inactivo")
+    return user
 
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
     user_db = users_db.get(form.username)
     if not user_db:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto", headers={"WWW-Authenticate": "Bearer"})
-    user = search_user(form.username)
+    user = search_user_db(form.username)
     if form.password != user.password:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="La contraseña no es correcta")
     return {"access_token": user.username, "token_type":"bearer"}
 
 @app.get("/users/me")
