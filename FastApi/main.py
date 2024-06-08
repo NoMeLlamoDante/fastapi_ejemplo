cb7f230005e375edc22295acfa2c1ac807b870af
NoMeLlamoDante <eo.zm24@outlook.com>
2024-06-07 22:00:02 -0600

    agregado plantilla para empezar a trabajar la conexión con monbodb

--- FastApi/main.py
+++ FastApi/main.py
@@ -1,6 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
-from routers import products, users, basic_auth_users, jwt_auth_users
+from routers import products, users, basic_auth_users, jwt_auth_users,usersdb
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
@@ -10,6 +10,7 @@ app.include_router(products.router)
 app.include_router(users.router)
 app.include_router(basic_auth_users.router)
 app.include_router(jwt_auth_users.router)
+app.include_router(usersdb.router)
 
 @app.get("/")
 async def root():
---
+++ FastApi/routers/usersdb.py
@@ -0,0 +1,63 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+
+router = APIRouter(prefix="/usersdb")
+
+#Entidad user
+class User(BaseModel):
+    id: int
+    name: str
+    surname: str
+    age: int
+
+
+users_list = [User(id= 1, name= "dante", surname= "zarate", age= 31),
+            User(id= 2,name= "cecilia", surname= "montejo", age= 31),
+            User(id= 3, name= "aimee", surname= "solis", age= 30)]
+
+@router.get("/user/{id}")
+async def user(id: int):
+    return search_user(id)
+    
+@router.get("/user/")
+async def userquery(id: int):
+    return search_user(id)
+
+def search_user(id: int):
+    users =  filter(lambda user:user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error": "no se ha encontrado el usuario solicitado"}
+
+@router.get("/")
+async def users():
+    return users_list
+    
+@router.post("/",response_model=User, status_code=201)
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        raise HTTPException(status_code=404,detail="El usuario ya existe")
+    users_list.append(user)
+    return user
+
+@router.put("/")
+async def user(user: User):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    if not found:
+        return {"error": "no se ha actualizado el usuario"}
+    return user
+
+@router.delete("/{id}")
+async def user(id: int):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+    if not found:
+        return {"error": "no se ha eliminado el usuario"}
