d8b207c8f32d0586d362f4c5d7e806d62d2777c0
NoMeLlamoDante <eo.zm24@outlook.com>
2024-06-07 21:08:16 -0600

    corrección sobre el main y los routers

--- FastApi/main.py
+++ FastApi/main.py
@@ -8,8 +8,8 @@ app = FastAPI()
 #Routers
 app.include_router(products.router)
 app.include_router(users.router)
-app.include_router(basic_auth_users)
-app.include_router(jwt_auth_users)
+app.include_router(basic_auth_users.router)
+app.include_router(jwt_auth_users.router)
 
 @app.get("/")
 async def root():
